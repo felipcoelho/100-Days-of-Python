255c2b46aa861d51fa45582bfb995d66ef12141c
Felipe Coelho <felipcoelho@gmail.com>
2025-09-08 12:51:47 -0300

    End of day 1

---
+++ Day 1/challenge.py
@@ -0,0 +1,8 @@
+# print the welcome message
+print("Welcome to the Band Name Generator.")
+# ask the user for the city they grew up in
+city = input("What's name of the city you grew up in?\n")
+# ask the user for the name of a pet
+pet = input("What's your pet's name?\n")
+# combine the name of their city and pet and show them their band name
+print("Your band name could be " + city + " " + pet)
---
+++ Day 1/task.py
@@ -0,0 +1,12 @@
+# Simple greeting program
+print("What's your name?")
+# Get user input
+name = input()
+# # Print greeting message
+# print("Hello " + name + "!")
+
+# Calculate the number of characters in the name
+name_length = str(len(name))
+
+#print the number of characters in the name
+print("Your name is " + name + " and it has " + name_length + " characters.")
